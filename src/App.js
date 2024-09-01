efe384bb522cc719f185eaa65668a6fb1bce02df
Ayala-Rozenberg <ayalad02120@gmail.com>
2024-09-01 18:22:49 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ZipCodeForm from './components/ZipCodeForm';
+import ErrorBoundary from './components/ErrorBoundary';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <ZipCodeForm/>
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path="my-app" element={<ZipCodeForm/>} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
